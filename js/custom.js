1c785eb511b7801147b6c9550d002d4435bfa297
陳德元 <r06521608@ntu.edu.tw>
2020-05-06 14:37:33 -0500

    add new map(develop use) and more doctor

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,81 @@
+// map
+jQuery(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+        ['LifeLong Dental Care - Berkeley', 37.8487121,-122.2720357],
+        ['LifeLong Pinole Health Center', 38.0054797,-122.3095352],
+        ['LifeLong Brookside Dental Care', 37.9538941,-122.3406842]
+        
+
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>LifeLong Dental Care - Berkeley</h3>' +
+        '<p>1860 Alcatraz Ave.Berkeley, CA 94703</p>' +        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>LifeLong Pinole Health Center</h3>' +
+        '<p>806 San Pablo Ave #1 Pinole, CA 94564</p>' +        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>LifeLong Brookside Dental Care</h3>' +
+        '<p>2023 Vale Rd.San Pablo, CA 94806s</p>' +
+        '</div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
+
+
+
+
 
 (function($) {
     // 'use strict';
